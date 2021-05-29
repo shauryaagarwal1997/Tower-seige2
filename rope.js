6a4f014c0c8960e6c9ab40dedfd0d5cf8f6b6419
shauryaagarwal1997 <75524612+shauryaagarwal1997@users.noreply.github.com>
2021-05-29 09:15:42 +0100

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,21 @@
+class Rope{
+    constructor(bodyA,pointB){
+        var options={
+        bodyA : bodyA,
+        pointB : pointB,
+        
+        stiffness:0.5,
+        length:50
+        }
+        this.constraint=Matter.Constraint.create(options);
+        World.add(world,this.constraint);
+    }
+    fly(){
+        this.constraint.bodyA=null;
+    }
+    display(){
+        if(this.constraint.bodyA != null){
+            line(this.constraint.bodyA.position.x,this.constraint.bodyA.position.y,this.constraint.pointB.x,this.constraint.pointB.y)
+        }
+    }
+}
